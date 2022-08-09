e65aeea73ff40c995706774cacf30ec2ef88fb76
YubiQuinzon <yjquinzon32@gmail.com>
2022-08-09 12:48:59 -0700

    fix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <?php 
-$API_KEY = getenv('APIKEY');
-print "<h2>. $API_KEY . is Fun!</h2>";
+$API_KEY = getenv("APIKEY");
+echo "<h2>"  .$API_KEY. "is Fun!</h2>";
 include_once("index.html"); 
 ?>
