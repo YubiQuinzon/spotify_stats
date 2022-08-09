80e2a6792f354e6d7e625533ca2a20f25107e5f6
YubiQuinzon <yjquinzon32@gmail.com>
2022-08-09 13:00:50 -0700

    add api_key

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 
+<script type="text/javascript">
+    var API_KEY = "<?php $API_KEY = getenv("API_KEY");; ?>";
+</script>
+
 <?php 
-$API_KEY = getenv("API_KEY");
-echo "<h2>"  .$API_KEY. "is Fun!</h2>";
 include_once("index.html"); 
 ?>
--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 var redirect_uri = 'https://yq-spotify-stats.herokuapp.com/'  //'http://127.0.0.1:5500/index.html'
 
 var client_id = 'f0cd086c71734ab287d5bac2266b52cc';
-var client_secret = API_KEY;
+var client_secret = '<?=$API_KEY?>';
 
 const AUTHORIZE = 'https://accounts.spotify.com/authorize'
 const TOKEN = 'https://accounts.spotify.com/api/token';
