42cd3c5b2166d568db35fd319208bce5f0e68e2b
YubiQuinzon <yjquinzon32@gmail.com>
2022-08-09 13:04:45 -0700

    fix

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-
-<script type="text/javascript">
-    var API_KEY = "<?php $API_KEY = getenv("API_KEY");; ?>";
-</script>
-
 <?php 
 $API_KEY = getVar("API_KEY");
 include_once("index.html"); 
 ?>
+
+<script type="text/javascript">
+    var API_KEY = "<?php $API_KEY = getenv("API_KEY");; ?>";
+</script>
