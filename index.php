38d1807c0d9a12b68a4b261c5a1b693d98c6a157
YubiQuinzon <yjquinzon32@gmail.com>
2022-08-09 13:16:45 -0700

    nah

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
+<script type="text/javascript">
+    var API_KEY = "<?php echo $API_KEY; ?>";
+</script>
+
 <?php 
 $API_KEY = getenv("API_KEY");
 include("index.html"); 
 ?>
 
-<script type="text/javascript">
-    var API_KEY = "<?php echo $API_KEY; ?>";
-</script>
